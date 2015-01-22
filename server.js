4116f7e1cde756af63888b02ccf77a5504aedc21
Simon Kim <simon.ty.kim@gmail.com>
2015-01-22 14:52:56 -0800

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Geolocates the user using latitude and longitude and then using weather undeground's API,
+//determines if it is currently snowing, in all forms (light, heavy, etc.), and then tells
+//the user either yes, I want to build a snowman or no, I don't want to and you should go away.
 app.post("/", function(req, res) {
   var latitude = req.body.lat;
   var longitude = req.body.lon;
   var key = (process.env.WUNDERAPI || "0adbd50f7a0e4896";
+  //API url
   var wunderURL = "http://api.wunderground.com/api/" + key +
       "/geolookup/conditions/q/" + latitude + "," + longitude + ".json";
 
@@ -17,10 +21,12 @@ app.post("/", function(req, res) {
     .get(wunderURL)
     .end(function(err, wunderData) {
       var answer;
+      //JSON is parsed here into readable text
       var parsedData = JSON.parse(wunderData.text);
 
       var weather = parsedData.current_observation.weather;
-
+      
+      //determines if it is snowing based on user's lat and long
       if(weather !== "Snow" || "Light Snow" || "Heavy Snow" || "Snow Showers" ||
         "Light Snow Showers" || "Heavy Snow Showers") {
         answer = "No. Go away, dude.";
